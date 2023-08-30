019c688f8be43ae40a2ea4d038fb5c6316a5cedc
sefi97 <jfariza1997@gmail.com>
2023-08-30 12:12:30 +0200

    Create razor project

---
+++ BulkuWebRazor/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using BulkuWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkuWebRazor.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+    }
+}
