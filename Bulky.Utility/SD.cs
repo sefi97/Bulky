3ab6368967cc49107a744cd4da12a9982922ab70
sefi97 <jfariza1997@gmail.com>
2023-09-06 18:07:56 +0200

    Add Identity

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bulky.Utility
+﻿namespace Bulky.Utility
 {
     public static class SD
     {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Conmpany";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
