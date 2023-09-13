aec7db6317d3c8ac2d9d33fadae8196f55c38b75
sefi97 <jfariza1997@gmail.com>
2023-09-13 16:03:25 +0200

    Add db initialization

---
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,60 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception e) { }
+
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "jose@admin.com",
+                    Email = "jose@admin.com",
+                    Name = "Jose",
+                    PhoneNumber = "XXX",
+                    StreetAddress = "XXX",
+                    State = "XXX",
+                    PostalCode = "XXX",
+                    City = "XXX"
+                }, "Admin1*").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "jose@admin.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Bulky.Utility;
 using Stripe;
 using Microsoft.Extensions.DependencyInjection;
+using Bulky.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,6 +41,7 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
@@ -66,9 +68,20 @@ app.UseAuthorization();
 
 app.UseSession();
 
+SeedDataBase();
+
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDataBase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
